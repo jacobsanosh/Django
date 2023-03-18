10852374344241b35bddfe0de77d477fddea972c
jacobsanosh <jacobsanosh@gmail.com>
2023-03-18 12:27:52 +0530

    showing data

--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . models import Topic,Webpage 
 # Create your views here.
 def index(request):
-    my__dict={"data":"hello hi"}
+    # my__dict={"data":"hello hi"}
+    data=Topic.objects.values_list('top_name')
+    my__dict={'data':data}
+    print(my__dict)
     return render(request,'index.html',context=my__dict)
     # return HttpResponse("hello")
